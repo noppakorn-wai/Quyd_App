ff1be5a6f9486241c9d1a9bc611c6b7519ac8057
Wait <noppakornc@hotmail.com>
2014-07-10 13:52:52 +0700

    - commit for merge

---
+++ Quyd/Quyd/Models/QuydUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Windows.Media.Imaging;
+using Facebook;
+using Facebook.Client;
+using Parse;
+
+namespace Quyd.Models
+{
+    class QuydCurrentUser : ParseUser
+    {
+    }
+}
